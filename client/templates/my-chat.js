51528d442f13b0747eb31425d319a4f04f809e20
Sagi Nadir <saginadir@gmail.com>
2016-02-04 14:14:40 +0200

    Added form to create messages

--- client/templates/my-chat.js
+++ client/templates/my-chat.js
@@ -1,14 +1,20 @@
 // counter starts at 0
-Session.setDefault('counter', 0);
-
 Template.myChat.helpers({
   messages: Messages.find({})
 });
 
 Template.myChat.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
+  'submit .submit-new-message': (e) => {
+    e.preventDefault();
+
+    let message = {
+      value: e.target.message.value,
+      createdAt: new Date(),
+    };
+
+    Messages.insert(message);
+
+    e.target.message.value = '';
   }
 });
 
